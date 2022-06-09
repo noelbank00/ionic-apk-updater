ecd8fe1b552c4066f6248184b601af3f21fc208f
Noel <noel.bank@insource.hu>
2022-06-09 20:17:28 +0200

    Install AppUpdater

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import ApkUpdater, {Update} from 'cordova-plugin-apkupdater';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  remote = 'https://raw.githubusercontent.com/kolbasa/cordova-plugin-apkupdater-demo/master/update';
+
+  constructor(private http: HttpClient, public platform: Platform) {
+    platform.ready().then(this.update.bind(this)).catch(console.error);
+  }
+
+  async update() {
+    const manifest = await this.http.get<Update>(this.remote + '/update.json').toPromise();
+
+    const remoteVersion = manifest.app.version.code;
+    const installedVersion = (await ApkUpdater.getInstalledVersion()).version.code;
+
+    if (remoteVersion > installedVersion) {
+      console.log('Install needed');
+      // await ApkUpdater.download(
+      //   this.remote + '/update.zip',
+      //   {
+      //     zipPassword: 'aDzEsCceP3BPO5jy',
+      //     onDownloadProgress: console.log,
+      //     onUnzipProgress: console.log
+      //   }
+      // );
+      // await ApkUpdater.install();
+    }
+  }
 
 }
